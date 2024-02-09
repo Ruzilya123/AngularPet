5f0b3c6b0207747f423c635462fb5fcec4f6beb2
Salimova Ruzilya Azatovna <ruzilya-s-2006@mail.ru>
2024-02-09 19:47:25 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {CarComponent} from "./components/car/car.component";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // то как называется компонент в html
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'newtraining';
+
 }
---
+++ src/app/components/car/car.component.ts
@@ -0,0 +1,104 @@
+import {Component, OnInit} from '@angular/core';
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-car',
+  standalone: true,
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './car.component.html',
+  styleUrl: './car.component.css'
+})
+export class CarComponent implements OnInit {
+  car!: Car;
+
+  constructor() {
+  }
+
+
+  ngOnInit() {
+    this.car = {
+      name: 'KIA',
+      speed: 235,
+      model: 'RIO',
+      colors: {
+        car: 'Blue',
+        salon: 'Black',
+        wheels: 'Silver'
+      },
+      options: ['ABS', 'Автопилот', 'Авто паркинг']
+    }
+  }
+
+  addOption(option: string) {
+    this.car.options.unshift(option);
+    return false;
+  }
+
+  bmwSelect() {
+    this.car = {
+      name: 'BMW',
+      speed: 280,
+      model: 'M5',
+      colors: {
+        car: 'White',
+        salon: 'White',
+        wheels: 'Silver'
+      },
+      options: ['ABS', 'Автопилот']
+    }
+  }
+
+  audiSelect() {
+    this.car = {
+      name: 'AUDI',
+      speed: 240,
+      model: 'A8',
+      colors: {
+        car: 'Black',
+        salon: 'Black',
+        wheels: 'Silver'
+      },
+      options: ['ABS', 'Автопилот', 'Авто паркинг']
+    }
+  }
+
+  kiaSelect() {
+    this.car = {
+      name: 'KIA',
+      speed: 235,
+      model: 'RIO',
+      colors: {
+        car: 'Blue',
+        salon: 'Black',
+        wheels: 'Silver'
+      },
+      options: ['ABS']
+    }
+  }
+
+  carSelect(carName: string) {
+    if (carName === 'bmw') {
+      this.bmwSelect();
+    } else if (carName === 'audi') {
+      this.audiSelect();
+    } else if (carName === 'kia') {
+      this.kiaSelect();
+    }
+  }
+}
+
+interface Colors {
+  car: string,
+  salon: string,
+  wheels: string
+}
+
+interface Car {
+  name: string,
+  speed: number,
+  model: string,
+  colors: Colors,
+  options: string[]
+}
